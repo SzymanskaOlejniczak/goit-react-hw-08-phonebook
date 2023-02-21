3934205f30bc58c6b96f03f213079e4c509f8419
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-21 19:55:41 +0100

    Create filterSlice.js

---
+++ src/redux/filterSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  value: '',
+};
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+
+  reducers: {
+    filterContacts(state, action) {
+      state.value = action.payload;
+    },
+  },
+});
+
+export const { filterContacts } = filterSlice.actions;
+
+export const filterReducer = filterSlice.reducer;
