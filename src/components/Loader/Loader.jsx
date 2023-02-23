652936cc620b093dffa93feb6bd452961007282a
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-23 23:27:47 +0100

    Create Loader.jsx

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,17 @@
+import { Grid } from 'react-loader-spinner';
+import styles from './Loader.module.css';
+
+export function Loader() {
+  return (
+    <Grid
+      height="80"
+      width="80"
+      color="#5366ce"
+      ariaLabel="grid-loading"
+      radius="12.5"
+      wrapperStyle={{}}
+      wrapperClass={styles.grid}
+      visible={true}
+    />
+  );
+}
