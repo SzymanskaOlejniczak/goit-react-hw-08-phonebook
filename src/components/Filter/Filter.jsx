110b2742e963a7955bea17a6c3eb919a0824faf7
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-23 23:27:45 +0100

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,27 +1,26 @@
-import { filterContacts } from "../../redux/filterSlice";
-import {  useDispatch } from "react-redux";
+import { nanoid } from 'nanoid';
+import { changeFilter } from '../../redux/contacts/actions';
+import { useDispatch } from 'react-redux';
 import style from './Filter.module.css';
 
 export const Filter = () => {
-  const dispatch = useDispatch(); 
-
-  const changeFilter = (e) => {
-    const value =  e.target.value.toLowerCase() 
-    dispatch(filterContacts(value));
+  const dispatch = useDispatch();
+  const loginInputId = nanoid();
+  const filter = e => {
+    const value = e.target.value.toLowerCase();
+    dispatch(changeFilter(value));
   };
   return (
-    <label className={style.label}>
+    <label className={style.label}  htmlFor={loginInputId}>
       Find contacts by name
-      <input 
-        className={style.input} 
+      <input
+        className={style.input}
         type="text"
         name="filter"
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Enter name" 
-        onChange={changeFilter} />
+        title="Enter name"
+        onChange={filter}
+      />
     </label>
-  )
+  );
 };
-
-
-
