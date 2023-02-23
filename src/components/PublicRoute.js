c4147dee20fea4d054941b1a08384a251fb322f8
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-23 23:27:58 +0100

    Create PublicRoute.js

---
+++ src/components/PublicRoute.js
@@ -0,0 +1,8 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import {authSelectors} from '../redux/auth/selector';
+
+export function PublicRoute() {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  return <>{isLoggedIn ? <Navigate to="/" /> : <Outlet />}</>;
+}
