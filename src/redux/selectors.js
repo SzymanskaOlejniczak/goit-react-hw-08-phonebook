71b41c7fcd7d4925c5114e9e4b52f3278332d40c
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-23 23:29:17 +0100

    Delete selectors.js

--- src/redux/selectors.js
+++
@@ -1,4 +0,0 @@
-export const getContacts = state => state.contacts.items;
-export const getFilterValue = state => state.filter.value;
-export const getIsLoadingStatus = state => state.contacts.isLoading;
-export const getErrorStatus = state => state.contacts.error;
