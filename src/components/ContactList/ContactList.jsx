38f0caa34f4a0a24ab7b9caa9a1a234f5765d94a
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-23 23:27:42 +0100

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,45 +1,47 @@
-import { deleteContacts } from "../../redux/operations";
-import { useSelector, useDispatch } from "react-redux";
-import { getFilterValue, getContacts, getErrorStatus } from 'redux/selectors';
-import { FcEmptyTrash} from 'react-icons/fc';
+import { deleteContact } from '../../redux/contacts/operations';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  getFilter,
+  getContacts,
+  getError,
+} from 'redux/contacts/selectors';
+import { FcEmptyTrash } from 'react-icons/fc';
 import style from './ContactList.module.css';
 
 export const ContactList = () => {
   const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilterValue);
+  const filter = useSelector(getFilter);
   const dispatch = useDispatch();
-  const error = useSelector(getErrorStatus);
-  
+  const error = useSelector(getError);
+
   const filteredContacts = contacts.filter(
     contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase()) ||
       contact.number.replace(/-|\s/g, '').includes(filter.replace(/-|\s/g, ''))
   );
-  const deleteContact = data => {
-    dispatch(deleteContacts(data));
-};
+  const deleteContacts = data => {
+    dispatch(deleteContact(data));
+  };
 
-    return filteredContacts.length > 0 ? ( 
+  return filteredContacts.length > 0 ? (
     <ul className={style.list}>
       {filteredContacts.map(({ id, name, number }) => {
         return (
-        <li className={style.contact} 
-          key={id}>
-          <p>{name}:</p>
-          <p>{number}</p>
-          <button
-            className={style.btn}
-            type="button"
-            onClick={() => deleteContact(id)}
-          >
-            <span>Delete</span> <FcEmptyTrash />
-          </button>
-        </li>
-      )})}
+          <li className={style.contact} key={id}>
+            <p>{name}:</p>
+            <p>{number}</p>
+            <button
+              className={style.btn}
+              type="button"
+              onClick={() => deleteContacts(id)}
+            >
+              <span>Delete</span> <FcEmptyTrash />
+            </button>
+          </li>
+        );
+      })}
     </ul>
-    ): ((error && <p>Error: {error}</p> )|| <p>No contacts</p>
-    );
+  ) : (
+    (error && <p>Error: {error}</p>) || <p>No contacts</p>
+  );
 };
-
-
-
