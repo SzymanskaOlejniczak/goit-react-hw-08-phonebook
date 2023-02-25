a145187d41f9fc8d8aff9a959df63a32af8c0faf
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-25 10:22:59 +0100

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,26 +7,28 @@ import phonebook from '../../images/phonebook.jpg';
 export default function Home() {
   return (
     <>
-      
-        
-        <section title={'Welcom!'} className={s.hero}>
-        
-          <p className={s.p}>This is a contact storage application.</p>
-          <button className={s.heroButton}>
-            <img src={plus} alt="icon" width={42} className={s.icon} />
-            <NavLink to="/addcontact" className={s.heroLink}>
-              Add Contact
-            </NavLink>
-          </button>
-          <button className={s.heroButton}>
-            <img src={search} alt="icon" width={42} className={s.icon} />
-            <NavLink to="/filtercontact" className={s.heroLink}>
-              Filter Contact
-            </NavLink>
-          </button>
-          <img src={phonebook} alt="" min-width={330} height={300} className={s.hero} />
-        </section>
-      
+      <section title={'Welcom!'} className={s.hero}>
+        <p className={s.p}>This is a contact storage application.</p>
+        <button className={s.heroButton}>
+          <img src={plus} alt="icon" width={42} className={s.icon} />
+          <NavLink to="/addcontact" className={s.heroLink}>
+            Add Contact
+          </NavLink>
+        </button>
+        <button className={s.heroButton}>
+          <img src={search} alt="icon" width={42} className={s.icon} />
+          <NavLink to="/filtercontact" className={s.heroLink}>
+            Filter Contact
+          </NavLink>
+        </button>
+        <img
+          src={phonebook}
+          alt=""
+          min-width={330}
+          height={300}
+          className={s.hero}
+        />
+      </section>
     </>
   );
 }
