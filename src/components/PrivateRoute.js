3a2c8d5721f3dbf7bcc9937f25d391a1d2bb32b9
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-25 12:35:48 +0100

    Delete PrivateRoute.js

--- src/components/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { authSelectors } from '../redux/auth/selector';
-
-export function PrivateRoute() {
-  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
-  return <>{isLoggedIn ? <Outlet /> : <Navigate to="/login" />}</>;
-}
