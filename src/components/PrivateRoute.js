3208d5c99ea2bd5895e6c5bc84ea1d9349b69ea6
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-25 10:22:50 +0100

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,6 +1,6 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import {authSelectors} from '../redux/auth/selector';
+import { authSelectors } from '../redux/auth/selector';
 
 export function PrivateRoute() {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
