09a18267aa42801a23ace66bcd8d144933153f43
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-25 12:36:05 +0100

    Update selector.js

--- src/redux/auth/selector.js
+++ src/redux/auth/selector.js
@@ -1,7 +1,7 @@
 export const getIsLoggedIn = state => state.auth.data.isLoggedIn;
-export const getUseremail = state => state.auth.data.user.email;
-export const getUsername = state => state.auth.data.user.name;
-export const getIsAuthError = state => state.auth.error;
+const getUseremail = state => state.auth.data.user.email;
+const getUsername = state => state.auth.data.user.name;
+const getIsAuthError = state => state.auth.error;
 
 export const authSelectors = {
   getIsLoggedIn,
