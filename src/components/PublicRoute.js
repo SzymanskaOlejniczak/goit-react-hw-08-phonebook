bb0790ffe2c2afb7c7cf5e331bb335b01a9bc69f
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-25 12:35:51 +0100

    Delete PublicRoute.js

--- src/components/PublicRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { authSelectors } from '../redux/auth/selector';
-
-export function PublicRoute() {
-  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
-  return <>{isLoggedIn ? <Navigate to="/" /> : <Outlet />}</>;
-}
