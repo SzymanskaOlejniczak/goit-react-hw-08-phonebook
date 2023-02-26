6514c20986960bbb890311b3faf5e136a371015e
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-26 13:15:20 +0100

    Update selector.js

--- src/redux/auth/selector.js
+++ src/redux/auth/selector.js
@@ -1,4 +1,4 @@
-export const getIsLoggedIn = state => state.auth.data.isLoggedIn;
+const getIsLoggedIn = state => state.auth.data.isLoggedIn;
 const getUseremail = state => state.auth.data.user.email;
 const getUsername = state => state.auth.data.user.name;
 const getIsAuthError = state => state.auth.error;
