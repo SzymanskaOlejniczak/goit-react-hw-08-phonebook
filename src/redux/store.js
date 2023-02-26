145420d860210b489ab302ec05cc37e471a08fe5
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-26 13:30:48 +0100

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,24 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
-import { contactReducer } from './contacts/reducers/contacts';
-import { filterReducer } from './contacts/reducers/filter';
-import { loadingReducer } from './contacts/reducers/loading';
-import { errorReducer } from './contacts/reducers/error';
+import {
+  contactReducer,
+  filterReducer,
+  loadingReducer,
+  errorReducer,
+} from './contacts/reducers';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import { dataAuthReducer, errorAuthReducer } from './auth/reducer';
-import storage from 'redux-persist/lib/storage';
-import { persistStore, persistReducer } from 'redux-persist';
+import logger from 'redux-logger';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 const persistConfig = {
   key: 'auth',
@@ -25,10 +37,21 @@ const authReducer = combineReducers({
   data: persistedReducer,
   error: errorAuthReducer,
 });
+
+// const persistedReducer = persistReducer(persistConfig, authReducer);
+
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     auth: authReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(logger),
+  devTools: process.env.NODE_ENV !== 'production',
 });
+
 export const persistor = persistStore(store);
