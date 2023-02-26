0d8df93bc9645063b605648b8744e53c4445b402
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-26 13:58:16 +0100

    Delete filter.js

--- src/redux/contacts/reducers/filter.js
+++
@@ -1,6 +0,0 @@
-import { createReducer } from '@reduxjs/toolkit';
-import { changeFilter } from '../actions';
-
-export const filterReducer = createReducer('', {
-  [changeFilter]: (_, action) => action.payload,
-});
